a778f1fa807236e09e53959c5a8f698cbcb1b79e
masahito1997 <velonica.gale2227@gmail.com>
2021-01-20 19:25:17 +0900

    add SliderApp

---
+++ SliderApp/SliderApp/SliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppApp.swift
+//  SliderApp
+//
+//  Created by 大迫雅仁 on 2021/01/20.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
