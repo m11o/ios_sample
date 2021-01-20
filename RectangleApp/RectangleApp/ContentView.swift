b597df7b0281a622f2784c788a1121e97ad7a881
masahito1997 <velonica.gale2227@gmail.com>
2021-01-20 20:00:28 +0900

    add RectangleApp

---
+++ RectangleApp/RectangleApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RectangleApp
+//
+//  Created by 大迫雅仁 on 2021/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Rectangle().padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RectangleApp/RectangleApp/RectangleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleAppApp.swift
+//  RectangleApp
+//
+//  Created by 大迫雅仁 on 2021/01/20.
+//
+
+import SwiftUI
+
+@main
+struct RectangleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
